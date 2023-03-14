027d266d84e52edd39145fb4e1704254ec8dbcd5
suffal <abhishekshuklagola82@gmail.com>
2023-03-14 15:13:47 +0530

    final Commit

--- app/src/main/java/com/example/bmiclaculatermui/MainActivity.kt
+++ app/src/main/java/com/example/bmiclaculatermui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.bmiclaculatermui
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,6 +12,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.bmiclaculatermui.databinding.ActivityMainBinding
 import kotlin.math.round
 
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (isclear) {
             isclear = false
             binding.calculate.setText("calculate")
+            binding.weight.isEnabled=true
+            binding.hight.isEnabled=true
         }
     }
 
@@ -35,11 +42,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (View?.id) {
             R.id.calculate -> {
                 if (binding.hight.editableText.isEmpty() && binding.weight.editableText.isEmpty()) {
+                    binding.hight.requestFocus()
                     Toast.makeText(this, "please Enter weight and height", Toast.LENGTH_SHORT)
                         .show()
                 } else if (binding.hight.editableText.isEmpty()) {
                     Toast.makeText(this, "please enter the hieght", Toast.LENGTH_SHORT).show()
                 } else if (binding.weight.editableText.isEmpty()) {
+                    binding.weight.requestFocus()
                     Toast.makeText(this, "please Enter the weight", Toast.LENGTH_SHORT).show()
                 }
 
@@ -48,8 +57,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     binding.calculate.text = "calculate"
                     binding.textview1.setText("")
                     binding.textview2.setText("")
+
+                    binding.weight.isEnabled=true
+                    binding.hight.isEnabled=true
+
                     binding.hight.editableText.clear()
                     binding.weight.editableText.clear()
+
                     Toast.makeText(this, "clear now", Toast.LENGTH_SHORT).show()
                 } else if (binding.hight.editableText.toString()
                         .isNotEmpty() && binding.weight.editableText.toString().isNotEmpty()
@@ -58,8 +72,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         // inislize the variable
                         isclear = true
                         binding.calculate.setText("clear")
+
+                        binding.weight.isEnabled=false
+                        binding.hight.isEnabled=false
+
+
                         val hi = (binding.hight.editableText.toString().toDouble())
                         val wi = (binding.weight.editableText.toString().toDouble())
+
 
                         if (hi == 0.0 || wi == 0.0) {
                             Toast.makeText(this, "Invalid value", Toast.LENGTH_SHORT).show()
@@ -100,17 +120,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             return super.onCreateOptionsMenu(menu)
         }
 
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
             when (item.itemId) {
                 R.id.item1 -> {
-                    val intent= Intent(this,thirdActivity::class.java)
+                    val intent = Intent(this, thirdActivity::class.java)
                     startActivity(intent)
                     Toast.makeText(this, "about developer", Toast.LENGTH_SHORT).show()
                     return true
                 }
                 R.id.item2 -> {
-                    val intent= Intent(this,secoundActivity::class.java)
+                    val intent = Intent(this, secoundActivity::class.java)
                     startActivity(intent)
                     Toast.makeText(this, "BMI Chart", Toast.LENGTH_SHORT).show()
                     return true
@@ -118,15 +140,112 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 R.id.item3 -> {
                     finish()
                     System.exit(0)
-                    Toast.makeText(this, "closed app", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "closed app", Toast.LENGTH_SHORT).show()
                     return true
                 }
+
+                R.id.item4 -> {
+                   // val intent = Intent(this, secoundActivity::class.java)
+                   // startActivity(intent)
+                    Toast.makeText(this, "contect to user", Toast.LENGTH_SHORT).show()
+                    return true
+                }
+
+
+                R.id.item5 -> {
+                    // val intent = Intent(this, secoundActivity::class.java)
+                    // startActivity(intent)
+                    Toast.makeText(this, "what is BMI", Toast.LENGTH_SHORT).show()
+
+                    val intent= Intent(Intent.ACTION_VIEW, Uri.parse("https://www.cdc.gov/healthyweight/assessing/bmi/index.html"))
+                    startActivity(intent)
+
+                }
+
+
+
+
+                R.id.subitem1 -> {
+                    // val intent = Intent(this, secoundActivity::class.java)
+                    // startActivity(intent)
+                    Toast.makeText(this, "Dail phone no. of developer", Toast.LENGTH_SHORT).show()
+
+                    val intent=Intent(Intent.ACTION_DIAL, Uri.parse("tel:7570077927"))
+                    startActivity(intent)
+                    return true
+                }
+
+
+
+
+                R.id.subitem2 -> {
+                    // val intent = Intent(this, secoundActivity::class.java)
+                    // startActivity(intent)
+                    Toast.makeText(this, "calling to user", Toast.LENGTH_SHORT).show()
+
+
+                    if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED){
+
+                        val intent=Intent(Intent.ACTION_CALL)
+                        intent.data=Uri.parse("tel:6387511508")
+                        startActivity(intent)
+
+
+                    }
+                    else{
+                        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 1001)
+                    }
+                }
+
+
+
+
+
+                R.id.subitem3 -> {
+                    Toast.makeText(this, "send to email", Toast.LENGTH_SHORT).show()
+
+                    val intent=Intent(Intent.ACTION_SENDTO).apply {
+                        data= Uri.parse("mailto:")
+                        putExtra(Intent.EXTRA_EMAIL, arrayOf("abhishekshuklagola82@gmail.com"))
+                      //  putExtra(Intent.EXTRA_SUBJECT,"my work is good")
+                    }
+                    startActivity(intent)
+
+                }
+
+
+
+
+
+
+
+
+
+
+
             }
+
+
             return super.onOptionsItemSelected(item)
         }
 
-    override fun onBackPressed() {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==1001){
+            if(grantResults.isNotEmpty() && permissions[0].equals(PackageManager.PERMISSION_GRANTED)){
 
+            }else{
+                Toast.makeText(this, "Please give permission", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+      Toast.makeText(this,"please go to option menu and press the exit option",Toast.LENGTH_LONG).show()
     }
 
     }
